ca3883de8ca6319cee41254cb295a499f4072cc5
ulyazen <ulyazen@gmail.com>
2021-12-07 22:15:42 +0700

    test style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,14 +15,14 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    <link rel="icon" href="{{ asset('assets/img/brand/favicon.png') }}" type="image/png">
+    <link rel="icon" href="{{ secure_asset('assets/img/brand/favicon.png') }}" type="image/png">
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
     <!-- Icons -->
-    <link rel="stylesheet" href="{{ asset('/assets/vendor/nucleo/css/nucleo.css ')}}" type="text/css">
-    <link rel="stylesheet" href="{{ asset('/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{ secure_asset('/assets/vendor/nucleo/css/nucleo.css ')}}" type="text/css">
+    <link rel="stylesheet" href="{{ secure_asset('/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css')}}" type="text/css">
     <!-- Argon CSS -->
-    <link rel="stylesheet" href="{{ asset('/assets/css/argon.css?v=1.2.0')}}" type="text/css">
+    <link rel="stylesheet" href="{{ secure_asset('/assets/css/argon.css?v=1.2.0')}}" type="text/css">
 
 
 </head>
@@ -32,7 +32,7 @@
         <nav id="navbar-main" class="navbar navbar-horizontal navbar-transparent navbar-main navbar-expand-lg navbar-light">
             <div class="container">
                 <a class="navbar-brand" href="dashboard.html">
-                    <img src="{{ asset('assets/img/brand/white.png') }}">
+                    <img src="{{ secure_asset('assets/img/brand/white.png') }}">
                 </a>
 
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -92,13 +92,13 @@
             @yield('content')
         </main>
     </div>
-    <script src="{{ asset('assets/vendor/jquery/dist/jquery.min.js') }}"></script>
-    <script src="{{ asset('assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('assets/vendor/js-cookie/js.cookie.js') }}"></script>
-    <script src="{{ asset('assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js') }}"></script>
-    <script src="{{ asset('assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js') }}"></script>
+    <script src="{{ secure_asset('assets/vendor/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ secure_asset('assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ secure_asset('assets/vendor/js-cookie/js.cookie.js') }}"></script>
+    <script src="{{ secure_asset('assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js') }}"></script>
+    <script src="{{ secure_asset('assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js') }}"></script>
     <!-- Argon JS -->
-    <script src="{{ asset('assets/js/argon.js?v=1.2.0') }}"></script>
+    <script src="{{ secure_asset('assets/js/argon.js?v=1.2.0') }}"></script>
 </body>
 
 </html>
