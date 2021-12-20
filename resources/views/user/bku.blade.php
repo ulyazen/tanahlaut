ce71c28a6d2f3ce7ca0fcca3f1284cf7879ebaac
Adriantosetyo <86601495+Adriantosetyo@users.noreply.github.com>
2021-12-20 19:24:39 +0700

    Update bku.blade.php

--- resources/views/user/bku.blade.php
+++ resources/views/user/bku.blade.php
@@ -15,12 +15,12 @@
             <div class="card-body bg-transparent">
                 <div class="row align-items-center">
                     <div class="col">
-                        <h3 class="h3 mb-0">Saldo Bulan Lalu</h3>
+                        <h3 class="h3 mb-0">Januari</h3>
                     </div>
                     <div class="col">
                         <ul class="nav nav-pills justify-content-end">
                             <li class="nav-item mr-2 mr-md-0">
-                                <a href="{{route('user.bku.sdl', Auth::user()->id)}}"
+                                <a href="{{route('user.bku.bakujan', Auth::user()->id)}}"
                                     class="nav-link py-2 px-3 active">
                                     <span class="d-md-block">Details</span>
                                 </a>
@@ -34,12 +34,12 @@
             <div class="card-body bg-transparent">
                 <div class="row align-items-center">
                     <div class="col">
-                        <h3 class="h3 mb-0">Belanja Barang dan Jasa</h3>
+                        <h3 class="h3 mb-0">Februari</h3>
                     </div>
                     <div class="col">
                         <ul class="nav nav-pills justify-content-end">
                             <li class="nav-item mr-2 mr-md-0">
-                                <a href="{{route('user.bku.bbj', Auth::user()->id)}}"
+                                <a href="{{route('user.bku.bkufeb', Auth::user()->id)}}"
                                     class="nav-link py-2 px-3 active">
                                     <span class="d-md-block">Details</span>
                                 </a>
@@ -52,12 +52,12 @@
             <div class="card-body bg-transparent">
                 <div class="row align-items-center">
                     <div class="col">
-                        <h3 class="h3 mb-0">Saldo Tunai</h3>
+                        <h3 class="h3 mb-0">Maret</h3>
                     </div>
                     <div class="col">
                         <ul class="nav nav-pills justify-content-end">
                             <li class="nav-item mr-2 mr-md-0">
-                                <a href="{{route('user.bku.st', Auth::user()->id)}}"
+                                <a href="{{route('user.bku.bkumar', Auth::user()->id)}}"
                                     class="nav-link py-2 px-3 active">
                                     <span class="d-md-block">Details</span>
                                 </a>
@@ -66,16 +66,16 @@
                     </div>
                 </div>
             </div>
-                    <div class="card">
+            <div class="card">
             <div class="card-body bg-transparent">
                 <div class="row align-items-center">
                     <div class="col">
-                        <h3 class="h3 mb-0">Belanja Modal</h3>
+                        <h3 class="h3 mb-0">April</h3>
                     </div>
                     <div class="col">
                         <ul class="nav nav-pills justify-content-end">
                             <li class="nav-item mr-2 mr-md-0">
-                                <a href="{{route('user.bku.bm', Auth::user()->id)}}"
+                                <a href="{{route('user.bku.bkuapr', Auth::user()->id)}}"
                                     class="nav-link py-2 px-3 active">
                                     <span class="d-md-block">Details</span>
                                 </a>
@@ -83,6 +83,150 @@
                         </ul>
                     </div>
                 </div>
+                 <div class="card mt-6">
+            <div class="card-body bg-transparent">
+                <div class="row align-items-center">
+                    <div class="col">
+                        <h3 class="h3 mb-0">Mei</h3>
+                    </div>
+                    <div class="col">
+                        <ul class="nav nav-pills justify-content-end">
+                            <li class="nav-item mr-2 mr-md-0">
+                                <a href="{{route('user.bku.bakumei', Auth::user()->id)}}"
+                                    class="nav-link py-2 px-3 active">
+                                    <span class="d-md-block">Details</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+             <div class="card mt-6">
+            <div class="card-body bg-transparent">
+                <div class="row align-items-center">
+                    <div class="col">
+                        <h3 class="h3 mb-0">Juni</h3>
+                    </div>
+                    <div class="col">
+                        <ul class="nav nav-pills justify-content-end">
+                            <li class="nav-item mr-2 mr-md-0">
+                                <a href="{{route('user.bku.bakujun', Auth::user()->id)}}"
+                                    class="nav-link py-2 px-3 active">
+                                    <span class="d-md-block">Details</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+             <div class="card mt-6">
+            <div class="card-body bg-transparent">
+                <div class="row align-items-center">
+                    <div class="col">
+                        <h3 class="h3 mb-0">Juli</h3>
+                    </div>
+                    <div class="col">
+                        <ul class="nav nav-pills justify-content-end">
+                            <li class="nav-item mr-2 mr-md-0">
+                                <a href="{{route('user.bku.bakujuli', Auth::user()->id)}}"
+                                    class="nav-link py-2 px-3 active">
+                                    <span class="d-md-block">Details</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+             <div class="card mt-6">
+            <div class="card-body bg-transparent">
+                <div class="row align-items-center">
+                    <div class="col">
+                        <h3 class="h3 mb-0">Agustus</h3>
+                    </div>
+                    <div class="col">
+                        <ul class="nav nav-pills justify-content-end">
+                            <li class="nav-item mr-2 mr-md-0">
+                                <a href="{{route('user.bku.bakuags', Auth::user()->id)}}"
+                                    class="nav-link py-2 px-3 active">
+                                    <span class="d-md-block">Details</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+             <div class="card mt-6">
+            <div class="card-body bg-transparent">
+                <div class="row align-items-center">
+                    <div class="col">
+                        <h3 class="h3 mb-0">September</h3>
+                    </div>
+                    <div class="col">
+                        <ul class="nav nav-pills justify-content-end">
+                            <li class="nav-item mr-2 mr-md-0">
+                                <a href="{{route('user.bku.bakusep', Auth::user()->id)}}"
+                                    class="nav-link py-2 px-3 active">
+                                    <span class="d-md-block">Details</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+             <div class="card mt-6">
+            <div class="card-body bg-transparent">
+                <div class="row align-items-center">
+                    <div class="col">
+                        <h3 class="h3 mb-0">Oktober</h3>
+                    </div>
+                    <div class="col">
+                        <ul class="nav nav-pills justify-content-end">
+                            <li class="nav-item mr-2 mr-md-0">
+                                <a href="{{route('user.bku.bakuokt', Auth::user()->id)}}"
+                                    class="nav-link py-2 px-3 active">
+                                    <span class="d-md-block">Details</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+             <div class="card mt-6">
+            <div class="card-body bg-transparent">
+                <div class="row align-items-center">
+                    <div class="col">
+                        <h3 class="h3 mb-0">November</h3>
+                    </div>
+                    <div class="col">
+                        <ul class="nav nav-pills justify-content-end">
+                            <li class="nav-item mr-2 mr-md-0">
+                                <a href="{{route('user.bku.bakunov', Auth::user()->id)}}"
+                                    class="nav-link py-2 px-3 active">
+                                    <span class="d-md-block">Details</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+             <div class="card mt-6">
+            <div class="card-body bg-transparent">
+                <div class="row align-items-center">
+                    <div class="col">
+                        <h3 class="h3 mb-0">Desember</h3>
+                    </div>
+                    <div class="col">
+                        <ul class="nav nav-pills justify-content-end">
+                            <li class="nav-item mr-2 mr-md-0">
+                                <a href="{{route('user.bku.bakudes', Auth::user()->id)}}"
+                                    class="nav-link py-2 px-3 active">
+                                    <span class="d-md-block">Details</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
             </div>
         </div>
     </div>
